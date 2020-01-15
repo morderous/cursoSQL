966b1b9401f3dbc00e0af46696273067cff68e09
Lucas Hubes <lucashubes@gmail.com>
2020-01-15 17:31:08 -0300

    adicionando modelo relacional

--- inserts comercio.sql
+++ inserts comercio.sql
@@ -7,7 +7,8 @@ desc telefone;
 INSERT INTO `endereco` (`Rua`,`Bairro`,`Cidade`,`Estado`,`fk_cliente`) VALUES ("Randall U. Mills","Tapajós","Blumenau","SC",1),("Azalia N. Mcdonald","Tapajós","Salvador","BA",2),("Chandler V. Haney","Tapajós","Colombo","PR",3),("Oprah P. Drake","Tapajós","Juazeiro","BA",4),("Quin R. Workman","Tapajós","Piracicaba","SP",5),("Jeremy H. Waller","Tapajós","Caucaia","CE",6),("Deanna H. Wiggins","Tapajós","Canoas","RS",7),("Noelle X. Gilliam","Nova Brasilia","Colombo","PR",8),("Lane G. Sellers","Nova Brasilia","Belford Roxo","RJ",9),("Castor T. Schmidt","Nova Brasilia","Ribeirão Preto","SP",10);
 INSERT INTO `endereco` (`Rua`,`Bairro`,`Cidade`,`Estado`,`fk_cliente`) VALUES ("Myra R. Chase","Nova Brasilia","Cabo de Santo Agostinho","PE",11),("Dolan E. Mendez","Nova Brasilia","Chapecó","SC",12),("Katelyn T. Weiss","Nova Brasilia","Sousa","PB",13),("Vladimir I. Finch","Nova Brasilia","Ribeirão Preto","SP",14),("Jocelyn C. Guerra","Carijós","Petrópolis","RJ",15),("Boris G. Bolton","Carijós","Chapecó","SC",16),("Autumn C. Ellis","Carijós","Governador Valadares","MG",17),("Brittany L. Stewart","Carijós","Carapicuíba","SP",18),("Kimberley N. Richmond","Carijós","Duque de Caxias","RJ",19),("Virginia K. Reese","Carijós","Belford Roxo","RJ",20);
 
-
+INSERT INTO `telefone` (`Numero`,`Tipo`,`fk_cliente`) VALUES ("(89)8959-9621","COM",18),("(63)5170-2998","COM",18),("(25)8896-6343","COM",14),("(72)0697-6529","RES",19),("(25)5594-4353","RES",4),("(46)4099-6392","RES",5),("(18)0702-5976","CEL",1),("(10)2601-7039","CEL",6),("(44)0860-7465","CEL",10),("(50)5622-5467","COM",2);
+INSERT INTO `telefone` (`Numero`,`Tipo`,`fk_cliente`) VALUES ("(85)8485-2888","CEL",20),("(64)6787-5583","CEL",15),("(80)7682-6645","COM",17),("(22)5355-0328","COM",15),("(37)6170-0116","COM",12),("(68)8667-0997","RES",18),("(59)4568-0846","RES",16),("(65)7129-3352","RES",16),("(63)3250-2492","CEL",3),("(82)6761-2873","CEL",16);
 
 
 
